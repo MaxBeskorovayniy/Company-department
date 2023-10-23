ec6ab5f53db8d98a9c825a852b1537043ba05bef
Максим Бескоровайный <>
2023-10-23 14:39:50 +0300

    created File123

---
+++ Company departments/File123.swift
@@ -0,0 +1,8 @@
+//
+//  File123.swift
+//  Company departments
+//
+//  Created by Максим Бескоровайный on 23.10.2023.
+//
+
+import Foundation
---
+++ Company departments/NewFile.swift
@@ -0,0 +1,8 @@
+//
+//  NewFile.swift
+//  Company departments
+//
+//  Created by Максим Бескоровайный on 23.10.2023.
+//
+
+import Foundation
